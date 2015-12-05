3ccd7ca35ad2f156960a79c02e0920d056aa139d
Lorenzo Mele <github@loman.it>
2015-12-05 19:44:00 +0100

    day 05 - first version

---
+++ 05/code.py
@@ -0,0 +1,25 @@
+
+nice = 0
+naughty_strings = ("ab", "cd", "pq", "xy")
+
+with open("input", "r") as name_list:
+	for name in name_list:
+		naughty = False
+		for ns in naughty_strings:
+			if ns in name:
+				naughty = True
+		if not naughty:
+			vowels = 0
+			prev_c = ""
+			twice_letter = False
+			for c in name:
+				if c in "aeiou": vowels += 1
+				if c == prev_c:
+					twice_letter = True
+				prev_c = c
+			if vowels >= 3 and twice_letter:
+				nice += 1
+
+if nice != 258:
+	print("Didn't work")
+print("Nice names: {}".format(nice))
