87be23d4d149760cd096b0d3399a1b763f5d0c46
Lorenzo Mele <github@loman.it>
2015-12-05 20:06:35 +0100

    second part of day 05

--- 05/code.py
+++ 05/code.py
@@ -20,4 +20,25 @@ with open("input", "r") as name_list:
 			if vowels >= 3 and twice_letter:
 				nice += 1
 
-print("Nice names: {}".format(nice))
+print("Nice names, first version: {}".format(nice))
+
+
+
+nice = 0
+
+with open("input", "r") as name_list:
+	for name in name_list:
+		double_couple = False
+		sandwich_letter = False
+		for i in range(1,len(name)):
+			couple = name[i-1]+name[i]
+			if couple in name[i+1:]:
+				double_couple = True
+		for i in range(2,len(name)):
+			if name[i-2] == name[i]:
+				sandwich_letter = True
+		if double_couple and sandwich_letter:
+			nice += 1
+
+print("Nice names, second version: {}".format(nice))
+
