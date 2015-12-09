581ef6762b01db9a979ec7bcf79eca7c6463c36a
Lorenzo Mele <github@loman.it>
2015-12-09 11:26:49 +0100

    day 08 - second problem

--- 08/code.py
+++ 08/code.py
@@ -1,12 +1,19 @@
-character_difference = 0
+import json
+
+first_difference = 0
+second_difference = 0
 
 with open("input", "r") as strings:
     for string in strings:
     	string = string.strip()
-    	print("{} is long {}".format(string,len(string)))
+    	print("\n{} is long {}".format(string,len(string)))
     	s2 = eval(string)
     	print("{} is long {}".format(s2,len(s2)))
+    	s3 = json.dumps(string)
+    	print("{} is long {}".format(s3,len(s3)))
 
-    	character_difference += len(string) - len( eval(string) )
+    	first_difference += len(string) - len( eval(string) )
+    	second_difference += len( json.dumps(string) ) - len(string)
 
-print("difference: {}".format( character_difference ))
+print("\ndifference: {}".format( first_difference ))
+print("second difference: {}".format( second_difference ))
