1f33c5df540740dea777c43d1957f0b86af56c56
Lorenzo Mele <github@loman.it>
2015-12-11 10:56:33 +0100

    day 11 - slightly performance improvement

--- 11/code.py
+++ 11/code.py
@@ -19,14 +19,6 @@ def next_password(string):
 		if 'o' in string: continue
 		if 'l' in string: continue
 
-		# increasing sequence
-		found = False
-		for i in range( 2, len(string) ):
-			if ord(string[i-2]) + 1 == ord(string[i-1]) and ord(string[i-1]) + 1 == ord(string[i]):
-				found = True
-		if not found:
-			continue
-
 		# two pairs
 		found = 0
 		i = 1
@@ -36,6 +28,14 @@ def next_password(string):
 				i += 1
 			i += 1
 		if found < 2:
+			continue
+
+		# increasing sequence
+		found = False
+		for i in range( 2, len(string) ):
+			if ord(string[i-2]) + 1 == ord(string[i-1]) and ord(string[i-1]) + 1 == ord(string[i]):
+				found = True
+		if not found:
 			continue
 
 		return string
