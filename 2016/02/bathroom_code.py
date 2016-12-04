9c63987b7d91910ded37707b699e6f4dbaba69b1
greenkey <greenkey@loman.it>
2016-12-04 18:48:37 +0100

    fourth star

--- 2016/02/bathroom_code.py
+++ 2016/02/bathroom_code.py
@@ -2,25 +2,35 @@
 
 import sys
 
-keypad = [
+keypad1 = [
     "123",
     "456",
     "789"
 ]
+keypad2 = [
+    "  1  ",
+    " 234 ",
+    "56789",
+    " ABC ",
+    "  D  "
+]
 
-position = (1,1)
+keypad = keypad2
+position = (2,0)
 code = ""
 
 with open("input", "r") as f:
     for line in f:
         for c in line.strip():
             if c == "U":
-                position = (max(position[0]-1,0), position[1])
+                np = (max(position[0]-1,0), position[1])
             elif c == "D":
-                position = (min(position[0]+1,2), position[1])
+                np = (min(position[0]+1,len(keypad)-1), position[1])
             elif c == "R":
-                position = (position[0], min(position[1]+1,2))
+                np = (position[0], min(position[1]+1,len(keypad[position[0]])-1))
             elif c == "L":
-                position = (position[0], max(position[1]-1,0))
+                np = (position[0], max(position[1]-1,0))
+            if keypad[np[0]][np[1]] != " ":
+                position = np
         code += keypad[position[0]][position[1]]
 print(code)
