db1d222384bd33348c0f69a594ef187c51f2951d
Mele <lorenzo.mele@vodafonetelematics.com>
2016-12-05 16:43:06 +0000

    removed all the map function, using list comprehension

--- 2016/03/possible_triangles.py
+++ 2016/03/possible_triangles.py
@@ -4,13 +4,13 @@ import sys, re
 # Part One
 
 def parse_line(line):
-	return list(map(int, re.split( " +", line.strip() ) ))
+	return [int(i) for i in re.split( " +", line.strip() ) ]
 
 def possible_triangle(sides):
 	return sum(sides)-max(sides) > max(sides)
 
 with open(sys.argv[1],'r') as f:
-	print(sum( map(possible_triangle, map(parse_line, [l for l in f] ) ) ))
+	print(sum( [possible_triangle(triangle) for triangle in [parse_line(l) for l in f] ] ))
 
 # Part Two
 
@@ -20,11 +20,11 @@ def sum_list(l1,l2):
 	return l1+l2
 
 def transpose(l):
-	return list(map(list, zip(*l)))
+	return [list(l) for l in zip(*l)]
 
 def parse_every_three_lines(lines):
-	return reduce(sum_list, map(transpose, [list(map(parse_line, lines[i-2:i+1])) if (i+1)%3==0 else [] for i in range(len(lines))]) )
+	return reduce(sum_list, [transpose(list([parse_line(l) for l in lines[i-2:i+1]])) if (i+1)%3==0 else [] for i in range(len(lines))]) 
 
 with open(sys.argv[1],'r') as f:
-	print(sum(map(possible_triangle, parse_every_three_lines( f.read().splitlines() ) )))
+	print(sum( [possible_triangle(triangle) for triangle in parse_every_three_lines( f.read().splitlines() ) ] ))
 	
