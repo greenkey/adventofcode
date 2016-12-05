d4e2df98f6f2f8ce910dc51c915d38ea1e9fdc3a
Mele <lorenzo.mele@vodafonetelematics.com>
2016-12-05 16:16:58 +0000

    sixth star

--- 2016/03/possible_triangles.py
+++ 2016/03/possible_triangles.py
@@ -1,11 +1,30 @@
 
 import sys, re
 
+# Part One
+
 def parse_line(line):
 	return list(map(int, re.split( " +", line.strip() ) ))
 
 def possible_triangle(sides):
 	return sum(sides)-max(sides) > max(sides)
 
-f = open(sys.argv[1],'r')
-print(sum( map(possible_triangle, map(parse_line, [l for l in f] ) ) ))
+with open(sys.argv[1],'r') as f:
+	print(sum( map(possible_triangle, map(parse_line, [l for l in f] ) ) ))
+
+# Part Two
+
+from functools import reduce
+
+def sum_list(l1,l2):
+	return l1+l2
+
+def transpose(l):
+	return list(map(list, zip(*l)))
+
+def parse_every_three_lines(lines):
+	return reduce(sum_list, map(transpose, [list(map(parse_line, lines[i-2:i+1])) if (i+1)%3==0 else [] for i in range(len(lines))]) )
+
+with open(sys.argv[1],'r') as f:
+	print(sum(map(possible_triangle, parse_every_three_lines( f.read().splitlines() ) )))
+	
