934a6bbba402ff97263c110d075423b4b58a0b93
Mele Lorenzo <lorenzo.mele@vodafonetelematics.com>
2016-12-16 18:12:39 +0000

    day 13: *

--- 2016/13/path_through_maze.py
+++ 2016/13/path_through_maze.py
@@ -5,21 +5,27 @@ class Maze():
 	def __init__(self,k):
 		self.k = k
 		self.maze = dict()
+		self.size = (0,0)
 
 	def __repr__(self):
-		boardsize = 30
-		out = ' ' + ''.join([str(i)[-1] for i in range(boardsize)]) + '\n'
-		for y in range(boardsize):
+		self.print_with_path(self)
+
+	def print_with_path(self,path=[]):
+		out = ' ' + ''.join([str(i)[-1] for i in range(self.size[0])]) + '\n'
+		for y in range(self.size[1]):
 			out += str(y)[-1]
-			for x in range(boardsize):
+			for x in range(self.size[0]):
 				if (x,y) not in self.maze:
 					out += ' '
+				elif (x,y) in path:
+					out += 'O'
 				else:
 					out += '.' if self.maze[(x,y)] else '#'
 			out += "\n"
 		return out
 
 	def calculate_free(self,x,y):
+		self.size = (max(self.size[0],x), max(self.size[1],y))
 		n = "{0:b}".format(x*x + 3*x + 2*x*y + y + y*y + self.k)
 		if sum([int(c) for c in n])%2 == 0:
 			return True
@@ -63,17 +69,33 @@ def find_paths(path_so_far,position_to_reach,maze):
 			for path in find_paths(path_so_far+[(p[0], p[1])], position_to_reach, maze):
 				yield path
 
+def find_paths2(initial_position,position_to_reach,maze):
+	paths_so_far = [[initial_position]]
+	while paths_so_far:
+		p = paths_so_far.pop()
+		print(maze.print_with_path(p))
+		#print(p)
+		#input()
+		(x,y) = p[-1]
+		for (x,y) in [(x+1, y),(x, y+1),(x-1, y),(x, y-1)]:
+			if x>=0 and y>=0 and maze.is_free_and_new(x, y):
+				if (x,y) == position_to_reach:
+					return p + [(x,y)]
+				paths_so_far.insert(0, p + [(x,y)] )
+		
+
 m = Maze(int(sys.argv[1]))
 position = (1,1)
 position_to_reach = tuple([int(i) for i in sys.argv[2].split(',')])
 
 min_path = None
-for p in find_paths([position],position_to_reach,m):
-	print("path found: {}".format(p))
-	print(m)
-	if not min_path:
-		min_path = p
-	elif len(min_path) > len(p):
-		min_path = p
+#for p in find_paths([position],position_to_reach,m):
+#	print("path found: {}".format(p))
+#	print(m)
+#	if not min_path:
+#		min_path = p
+#	elif len(min_path) > len(p):
+#		min_path = p
+min_path = find_paths2(position,position_to_reach,m)
 
-print("Found path with {} steps:\n{}".format(len(min_path), min_path))
+print("Found path with {} steps:\n{}".format(len(min_path)-1, min_path))
