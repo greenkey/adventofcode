71d9da60bc6c421d9b4e21c9100be67a8c4fb6a7
Mele Lorenzo <lorenzo.mele@vodafonetelematics.com>
2016-12-19 18:21:17 +0000

    Day 18: ** (even imporved performance)

--- 2016/18/tiles.py
+++ 2016/18/tiles.py
@@ -1,13 +1,17 @@
 
-def next_tile_row(row):
+def next_tile_row_old(row):
 	row = [1] + row + [1]
-	ret = []
 	for i in range(1,len(row)-1):
-		if row[i-1]!=row[i+1]:
-			ret.append(0)
-		else:
-			ret.append(1)
-	return ret
+		yield row[i-1]!=row[i+1]
+
+def next_tile_row(row):
+	prev2 = 1
+	prev1 = row[0]
+	for x in row[1:]:
+		yield prev2!=x
+		prev2 = prev1
+		prev1 = x
+	yield prev2!=1
 
 
 if __name__ == '__main__':
@@ -23,7 +27,8 @@ if __name__ == '__main__':
 
 	count_free = sum(row)
 	for i in range(int(sys.argv[2])-1):
-		row = next_tile_row(row)
+		print('\r{} '.format(i), end='')
+		row = list(next_tile_row(row))
 		count_free += sum(row)
 
 	#print('\n'.join(tiles))
