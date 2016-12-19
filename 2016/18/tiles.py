37c0a36542fbca919cbd14d305037d2a1d5edceb
Mele Lorenzo <lorenzo.mele@vodafonetelematics.com>
2016-12-19 17:54:08 +0000

    Day 18: ** (just imporved performance)

--- 2016/18/tiles.py
+++ 2016/18/tiles.py
@@ -1,13 +1,12 @@
 
 def next_tile_row(row):
-	row = '.' + row + '.'
-	ret = ''
+	row = [1] + row + [1]
+	ret = []
 	for i in range(1,len(row)-1):
-		buf = row[i-1:i+2]
-		if buf in ['^^.','.^^','^..','..^']:
-			ret += '^'
+		if row[i-1]!=row[i+1]:
+			ret.append(0)
 		else:
-			ret += '.'
+			ret.append(1)
 	return ret
 
 
@@ -16,13 +15,16 @@ if __name__ == '__main__':
 	import sys
 
 	try:
-		first_row = open(sys.argv[1]).read().strip()
+		row = open(sys.argv[1]).read().strip()
 	except:
-		first_row = sys.argv[1]
+		row = sys.argv[1]
 
-	tiles = [first_row]
+	row = [1 if c=='.' else 0 for c in row]
+
+	count_free = sum(row)
 	for i in range(int(sys.argv[2])-1):
-		tiles.append(next_tile_row(tiles[-1]))
+		row = next_tile_row(row)
+		count_free += sum(row)
 
 	#print('\n'.join(tiles))
-	print(sum([sum([1 if c=='.' else 0 for c in row]) for row in tiles]))
+	print(count_free)
