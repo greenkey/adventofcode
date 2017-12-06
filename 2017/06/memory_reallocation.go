20c68ac1195b5bd13185037a511f1642d82da9bf
Lorenzo Mele <lorenzo.mele@jobtome.com>
2017-12-06 07:15:21 +0100

    day 6 * go

---
+++ 2017/06/memory_reallocation.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strconv"
+	"strings"
+)
+
+func readFile(filename string) (numbers [][]int) {
+	dat, _ := ioutil.ReadFile("input")
+	lines := strings.Split(string(dat), "\n")
+	for _, line := range lines {
+		numberStrings := strings.Split(line, "\t")
+		numberInts := make([]int, len(numberStrings))
+		for i, s := range numberStrings {
+			numberInts[i], _ = strconv.Atoi(s)
+		}
+		numbers = append(numbers, numberInts)
+	}
+	return
+}
+
+func biggest(bank []int) (index, big int) {
+	for i, n := range bank {
+		if n > big {
+			index = i
+			big = n
+		}
+	}
+	return
+}
+
+func bankSignature(bank []int) (s string) {
+	bite := make([]byte, len(bank))
+	for i := range bank {
+		bite[i] = byte(bank[i])
+	}
+	return string(bite)
+}
+
+func main() {
+
+	banks := readFile("input")
+	for _, b := range banks {
+		combinations := make(map[string]int)
+		for {
+			if combinations[bankSignature(b)] == 1 {
+				break
+			}
+			combinations[bankSignature(b)] = 1
+			i, n := biggest(b)
+			b[i] = 0
+			for n > 0 {
+				i = (i + 1) % len(b)
+				b[i]++
+				n--
+			}
+		}
+		fmt.Println(len(combinations))
+	}
+
+}
