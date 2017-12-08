fe9aafa2605f151cfb5f9e11744265a6dd6f7f01
Lorenzo Mele <lorenzo.mele@jobtome.com>
2017-12-08 12:49:11 +0100

    day 8 * go

---
+++ 2017/08/execute.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+func readFile(filename string) (lines []string) {
+	dat, _ := ioutil.ReadFile("input")
+	lines = strings.Split(string(dat), "\n")
+	return
+}
+
+func condition(val1 int, cond string, val2 int) bool {
+	if cond == ">" {
+		return val1 > val2
+	}
+	if cond == "<" {
+		return val1 < val2
+	}
+	if cond == ">=" {
+		return val1 >= val2
+	}
+	if cond == "<=" {
+		return val1 <= val2
+	}
+	if cond == "==" {
+		return val1 == val2
+	}
+	if cond == "!=" {
+		return val1 != val2
+	}
+	return false
+}
+
+func execute(instructions []string, registers map[string]int) {
+	r, _ := regexp.Compile("^([a-z]+) (inc|dec) ([0-9-]+) if ([a-z]+) ([!<>=]+) ([0-9-]+)$")
+	for _, line := range instructions {
+		res := r.FindAllStringSubmatch(line, -1)[0]
+		condVal, _ := strconv.Atoi(res[6])
+		if condition(registers[res[4]], res[5], condVal) {
+			val, _ := strconv.Atoi(res[3])
+			if res[2] == "inc" {
+				registers[res[1]] += val
+			}
+			if res[2] == "dec" {
+				registers[res[1]] -= val
+			}
+		}
+	}
+}
+
+func main() {
+
+	lines := readFile("input")
+
+	registers := make(map[string]int)
+
+	execute(lines, registers)
+
+	biggestVal := 0
+	for _, v := range registers {
+		if v > biggestVal {
+			biggestVal = v
+		}
+	}
+	fmt.Println(biggestVal)
+
+}
