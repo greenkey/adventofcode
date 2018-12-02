48755581a79e34ba6beef4a0b3700d82184629fa
Lorenzo Mele <lorenzo.mele@cloudacademy.com>
2018-12-02 12:09:28 +0100

    day 2 star 2

--- 2018/02/solve.py
+++ 2018/02/solve.py
@@ -19,8 +19,23 @@ def get_checksum(box_list):
 
     return twos * threes
 
+def letterwise_and(word1, word2):
+    if len(word1) == len(word2):
+        for i in range(len(word1)):
+            if word1[i] == word2[i]:
+                yield word1[i]
+
+def find_the_two_boxes(box_list):
+    for i in range(len(box_list) - 1):
+        for j in range(i + 1, len(box_list)):
+            common = ''.join(letterwise_and(box_list[i], box_list[j]))
+            if len(common) == len(box_list[i]) - 1:
+                return common
+
 if __name__ == '__main__':
     with open('input') as f:
         box_list = [l for l in f.readlines()]
     
     print(get_checksum(box_list))
+
+    print(find_the_two_boxes(box_list))
