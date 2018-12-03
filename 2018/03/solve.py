325e7132cf1d7b9e230ea98597fb97ec0f4ae6fc
Lorenzo Mele <lorenzo.mele@cloudacademy.com>
2018-12-03 08:54:42 +0100

    day 3 star 2 (plus refactoring)

--- 2018/03/solve.py
+++ 2018/03/solve.py
@@ -2,17 +2,26 @@ import re
 from collections import defaultdict
 
 
-def contended_inches(squares):
+def create_map(squares):
     fabric = defaultdict(list)
-    max_x, max_y = 0, 0
     for name, start_x, start_y, width, height in squares:
         for x in range(start_x, start_x+width):
             for y in range(start_y, start_y+height):
                 fabric[(x,y)].append(name)
-                max_x = x if x > max_x else max_x
-                max_y = y if y > max_y else max_y
+    return fabric
 
-    return len([inch for inch in fabric.values() if len(inch) > 1])
+def contended_inches(fabric_map):
+    return len([inch for inch in fabric_map.values() if len(inch) > 1])
+
+def get_non_overlapping_clain(fabric_map):
+    all_names = set()
+    overlapping_names = set()
+    for inch in fabric_map.values():
+        all_names.update(inch)
+        if len(inch) > 1:
+            overlapping_names.update(inch)
+
+    return all_names - overlapping_names
 
 if __name__ == '__main__':
     # #1 @ 1,3: 4x4
@@ -23,4 +32,6 @@ if __name__ == '__main__':
             for line in f.readlines()
         ]
 
-    print(contended_inches(squares))
+    fabric_map = create_map(squares)
+    print(contended_inches(fabric_map))
+    print(get_non_overlapping_clain(fabric_map))
