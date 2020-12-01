210edda402f159d287eb2597edb4dce77cd70d5b
Lorenzo Mele <lorenzo.mele@gmail.com>
2020-12-01 10:12:06 +0100

    2020/01 **

--- 2020/01/solve.py
+++ 2020/01/solve.py
@@ -1,3 +1,4 @@
+from functools import reduce
 from itertools import combinations
 
 file_name = 'input'
@@ -7,4 +8,8 @@ with open(file_name, 'r') as f:
 
 for a, b in combinations(data, 2):
     if a + b == 2020:
-        print(a*b)
+        print(a * b)
+
+for nums in combinations(data, 3):
+    if sum(nums) == 2020:
+        print(reduce(lambda a, b: a * b, nums))
