3b669c766afcbb3b2ac4ca7631d0c162b231dd6a
Lorenzo Mele <lorenzo.mele@gmail.com>
2020-12-04 10:02:59 +0100

    2020/04 **

--- 2020/04/solve.py
+++ 2020/04/solve.py
@@ -1,8 +1,52 @@
 import re
+from functools import partial
 
 file_name = 'input'
 
-mask = re.compile(r'([a-z]+):([^ ]+)')
+document_mask = re.compile(r'([a-z]+):([^ ]+)')
+
+def is_int_between(lower, upper, x):
+    try:
+        num = int(x)
+    except TypeError:
+        return False
+    return lower <= num <= upper
+
+def matches_mask(mask, s):
+    return mask.findall(s)
+
+def validate_height(mask, hgt):
+    try:
+        height, mu = mask.findall(doc['hgt'])[0]
+    except IndexError:
+        return False
+
+    if mu == 'cm':
+        if not is_int_between(150, 193, height):
+            return False
+    elif mu == 'in':
+        if not is_int_between(59, 76, height):
+            return False
+
+    return True
+
+field_masks = {
+    'byr': partial(is_int_between, 1920, 2002),
+    'iyr': partial(is_int_between, 2010, 2020),
+    'eyr': partial(is_int_between, 2020, 2030),
+    'hgt': partial(validate_height, re.compile(r'([0-9]+)(cm|in)')),
+    'hcl': partial(matches_mask, re.compile(r'\#[0-9a-f]{6}')),
+    'ecl': partial(matches_mask, re.compile(r'(amb|blu|brn|gry|grn|hzl|oth)')),
+    'pid': partial(matches_mask, re.compile(r'^[0-9]{9}$')),
+}
+
+def validate(doc):
+    for field, check in field_masks.items():
+        if not check(doc[field]):
+            print(f'{field} is not valid')
+            return False
+    return True
+
 
 documents = []
 with open(file_name, 'r') as f:
@@ -12,10 +56,20 @@ with open(file_name, 'r') as f:
         if line:
             data.append(line)
         else:
-            documents.append({k: v for k, v in mask.findall(' '.join(data))})
+            documents.append({k: v for k, v in document_mask.findall(' '.join(data))})
             data = []
-    documents.append({k: v for k, v in mask.findall(' '.join(data))})
+    documents.append({k: v for k, v in document_mask.findall(' '.join(data))})
 
 required_fields = {'byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid'}
 
-print(sum(required_fields.issubset(doc.keys()) for doc in documents))
+valid_1 = 0
+valid_2 = 0
+for doc in documents:
+    if required_fields.issubset(doc.keys()):
+        print('='*80)
+        print(doc)
+        valid_1 += 1
+        valid_2 += validate(doc)
+
+print(valid_1)
+print(valid_2)
