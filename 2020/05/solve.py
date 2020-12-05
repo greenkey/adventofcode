2eeaed3d85d09f786f961c1a3f45b3e3e2719766
Lorenzo Mele <lorenzo.mele@gmail.com>
2020-12-05 10:11:20 +0100

    2020/05 **

--- 2020/05/solve.py
+++ 2020/05/solve.py
@@ -1,14 +1,19 @@
-import re
-from functools import partial
-
 file_name = 'input'
 
 def calculate_seat_id(ticket):
-    row = int(ticket[:7].replace('F', '0').replace('B', '1'), 2)
-    column = int(ticket[-3:].replace('L', '0').replace('R', '1'), 2)
-    return row * 8 + column
+    return int(ticket.replace('F', '0').replace('B', '1').replace('L', '0').replace('R', '1'), 2)
 
 with open(file_name, 'r') as f:
     tickets = f.readlines()
 
-print(max(calculate_seat_id(ticket) for ticket in tickets))
+ticket_ids = set(calculate_seat_id(ticket) for ticket in tickets)
+
+print(max(ticket_ids))
+
+for ticket_id in ticket_ids:
+    if ticket_id + 2 in ticket_ids and ticket_id + 1 not in ticket_ids:
+        print(ticket_id + 1)
+        break
+    elif ticket_id - 2 in ticket_ids and ticket_id - 1 not in ticket_ids:
+        print(ticket_id - 1)
+        break
