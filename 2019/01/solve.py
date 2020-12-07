2c52e2f1bcc82f69a2acf9d9cb84253baebe0bf4
Lorenzo Mele <lorenzo.mele@gmail.com>
2020-12-07 21:33:31 +0100

    2010/01 **

--- 2019/01/solve.py
+++ 2019/01/solve.py
@@ -1,5 +1,24 @@
 import sys
+from typing import Union
 
 file_name = sys.argv[1:] or 'input'
 
-print(sum(int(line) // 3 - 2 for line in open(file_name).readlines()))
+def calc_fuel(line: Union[str, int]) -> int:
+    return int(line) // 3 - 2
+
+def calc_additional_fuel(fuel: int) -> int:
+    additional_fuel = calc_fuel(fuel)
+    if additional_fuel > 0:
+        return additional_fuel + calc_additional_fuel(additional_fuel)
+    else:
+        return 0
+
+total_fuel = 0
+total_additional_fuel = 0
+for line in open(file_name).readlines():
+    fuel = calc_fuel(line)
+    total_fuel += fuel
+    total_additional_fuel += fuel + calc_additional_fuel(fuel)
+
+print(total_fuel)
+print(total_additional_fuel)
