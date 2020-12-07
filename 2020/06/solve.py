09b9deda6ad728b500f39fb7d0363e18ee757dc1
Lorenzo Mele <lorenzo.mele@gmail.com>
2020-12-07 12:13:31 +0100

    (remove useless code)

--- 2020/06/solve.py
+++ 2020/06/solve.py
@@ -4,9 +4,6 @@ from collections import Counter
 file_name = 'input'
 ascii_lowercase = set(string.ascii_lowercase)
 
-with open(file_name, 'r') as f:
-    tickets = f.readlines()
-
 data = open(file_name, 'r').read().split('\n\n')
 
 first_count = 0
