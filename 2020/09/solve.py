102dc86cd9cd58d341e361759225872ec86ad506
Lorenzo Mele <lorenzo.mele@gmail.com>
2020-12-09 10:13:25 +0100

    2020/09 **
    (cherry picked from commit ab180338391dfd5126bb6f2d1bfe8c583395c53b)

--- 2020/09/solve.py
+++ 2020/09/solve.py
@@ -9,18 +9,33 @@ def contains_sum(data: List[int], num: int) -> bool:
     return False
 
 
-def find_first_invalid(data: List[int], preamble: int) -> Optional[int]:
+def find_first_invalid(data: List[int], preamble: int) -> int:
     for i, num in enumerate(data):
         if i < preamble:
             continue
         if not contains_sum(data[i - preamble : i], num):
-            return num
+            return i
+    return -1
+
+
+def find_encryption_weakness(
+    data: List[int], first_invalid_position: int
+) -> Optional[int]:
+    for i in range(first_invalid_position):
+        for j in range(i, first_invalid_position):
+            if sum(data[i:j]) == data[first_invalid_position]:
+                return min(data[i:j]) + max(data[i:j])
     return None
 
 
 def solve(input_file: str):
     data = [int(x) for x in open(input_file).readlines()]
-    print(find_first_invalid(data, 25))
+
+    position = find_first_invalid(data, 25)
+    print(data[position])
+
+    encryption_weakness = find_encryption_weakness(data, position)
+    print(encryption_weakness)
 
 
 if __name__ == "__main__":
