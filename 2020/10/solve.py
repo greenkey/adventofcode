a3cf0ff0d2060a7c96bfad820e9316c8489c31e7
Lorenzo Mele <lorenzo.mele@revolut.com>
2020-12-10 17:38:42 +0100

    2020-10 **

--- 2020/10/solve.py
+++ 2020/10/solve.py
@@ -1,5 +1,8 @@
+import re
 import sys
 from collections import Counter
+from functools import lru_cache, reduce
+from typing import List, Tuple
 
 
 def solve(input_file: str):
@@ -12,38 +15,47 @@ def solve(input_file: str):
     diff_count = Counter(diffs)
     print(diff_count[1] * diff_count[3])
 
-    arrangements = set(tuple(l) for l in get_arrangements(diffs))
-    print(len(arrangements))
-    arrangements = list(get_arrangements_first([0] + adapters + [adapters[-1] + 3]))
-    print(len(arrangements))
+    print(count_arrangements("".join(str(x) for x in diffs)))
 
 
-def get_arrangements(diffs, prepend=None):
-    prepend = prepend or []
-    if not diffs:
-        return prepend
-    yield prepend + diffs
-    yield from get_arrangements(diffs[1:], prepend + [diffs[0]])
-    if diffs[:2] == [1, 1]:
-        yield from get_arrangements([2] + diffs[2:], prepend)
-    if diffs[:2] in ([2, 1], [1, 2]):
-        yield from get_arrangements(diffs[2:], prepend + [3])
+seen_combinations = dict()
 
 
-def get_arrangements_first(adapters, start=0):
-    if not adapters:
-        return None
-    for jump in (1, 2, 3):
-        try:
-            i = adapters.index(start + jump)
-        except ValueError:
-            continue
-        if not adapters[i+1:]:
-            yield [adapters[i]]
-        else:
-            for arrangement in get_arrangements_first(adapters[i+1:], adapters[i]):
-                yield [start] + arrangement
+def count_arrangements(diffs: str) -> int:
+    if len(diffs) <= 1:
+        return 1
+    if "3" in diffs:
+        changable = diffs.split("3")
+        return reduce(
+            lambda x, y: x * y, [count_arrangements(part) for part in diffs.split("3")]
+        )
+
+    try:
+        return seen_combinations[diffs]
+    except KeyError:
+        count = 0
+        if diffs[:2] in ("21", "12"):
+            count += count_arrangements("3" + diffs[2:])
+        if diffs[:2] in ("11"):
+            count += count_arrangements("2" + diffs[2:])
+        count += count_arrangements(diffs[1:])
+        seen_combinations[diffs] = count
+        return count
+
+
+def test_count_arrangements():
+    assert count_arrangements("1") == 1
+    assert count_arrangements("2") == 1
+    assert count_arrangements("3") == 1
+    assert count_arrangements("313") == 1
+    assert count_arrangements("11") == 2  # 11, 2
+    assert count_arrangements("21") == 2  # 21, 3
+    assert count_arrangements("12") == 2  # 12, 3
+    assert count_arrangements("3123") == 2  # 3123, 333
+    assert count_arrangements("121") == 3  # 121, 31, 13
+    assert count_arrangements("111") == 4  # 111, 21, 12, 3
+    assert count_arrangements("1111") == 7  # 1111, 211, 121, 112, 22, 31, 13
 
 
 if __name__ == "__main__":
-    solve(sys.argv[1] if len(sys.argv) > 1 else 'input')
+    solve(sys.argv[1] if len(sys.argv) > 1 else "input")
