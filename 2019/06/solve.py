c1fab96768f816baf6164b13596ffd60f5e72b29
Lorenzo Mele <lorenzo.mele@gmail.com>
2020-12-12 11:04:06 +0100

    2019/06 **

--- 2019/06/solve.py
+++ 2019/06/solve.py
@@ -11,6 +11,21 @@ def count_orbits(
     return count
 
 
+def transfers(reverse_map: Dict[str, List[str]], a: str, b: str) -> int:
+    path_a: List[str] = []
+    path_b: List[str] = []
+    while True:
+        a = reverse_map.get(a)
+        if a in path_b:
+            return len(path_a) + path_b.index(a)
+        path_a.append(a)
+
+        b = reverse_map.get(b)
+        if b in path_a:
+            return len(path_b) + path_a.index(b)
+        path_b.append(b)
+
+
 def solve(file_name: str = "input"):
     orbit_map = defaultdict(list)
     for relationship in open(file_name).readlines():
@@ -18,6 +33,9 @@ def solve(file_name: str = "input"):
         orbit_map[center_object].append(satellite)
 
     print(count_orbits(orbit_map))
+
+    reverse_map = {sat: grav for grav, sats in orbit_map.items() for sat in sats}
+    print(transfers(reverse_map, "YOU", "SAN"))
 
 
 if __name__ == "__main__":
