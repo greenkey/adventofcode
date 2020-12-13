630d7b141ff01c64f318307fec67ae8189cdc6b4
Lorenzo Mele <lorenzo.mele@gmail.com>
2020-12-13 22:30:30 +0100

    2020/13 **

--- 2020/13/solve.py
+++ 2020/13/solve.py
@@ -15,8 +15,26 @@ def solve(file_name: str):
 
     next_ts = min(next_ts_buses.keys())
     line = int(next_ts_buses[next_ts])
-
     print((next_ts - my_ts) * line)
+
+    shifts = [(int(line), i) for i, line in enumerate(bus_lines) if line != 'x']
+    freq2, shift2 = shifts.pop(0)
+    freq1, shift1 = shifts.pop(0)
+    ts = 1
+    while True:
+        if (ts + shift2) % freq2 == 0:
+            if (ts + shift1) % freq1 == 0:
+                freq2, shift2 = (freq1 * freq2, freq1 * freq2 - ts)
+                try:
+                    freq1, shift1 = shifts.pop(0)
+                except IndexError:
+                    break
+                continue
+            ts += freq2
+        else:
+            ts += 1
+
+    print(freq2 - shift2)
 
 
 if __name__ == "__main__":
