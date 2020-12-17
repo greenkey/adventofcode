1883ec4800d6070f3791498d8b59abecff1690dd
Lorenzo Mele <lorenzo.mele@gmail.com>
2020-12-17 09:04:18 +0100

    2020/17 ** (refactor)

--- 2020/17/solve.py
+++ 2020/17/solve.py
@@ -2,6 +2,30 @@ import sys
 from itertools import product
 
 
+def conway_n_d(space_2d, dimensions, keep_alives=(2, 3), newborn=3):
+    space = {tuple([x, y] + [0] * (dimensions - 2)): cube for (x, y), cube in space_2d.items()}
+    neighbors = list(product((-1, 0, 1), repeat=dimensions))
+    while True:
+        new_space = space.copy()
+        coord_to_check = {
+            tuple(c + n for c, n in zip(coord, neighbor))
+            for coord, cube in space.items() if cube
+            for neighbor in neighbors
+        }
+        for current in coord_to_check:
+            cube = space.get(current, 0)
+            count = sum(
+                space.get(tuple(c + n for c, n in zip(current, neighbor)), 0)
+                for neighbor in neighbors
+            ) - cube
+            if cube and count not in keep_alives:
+                new_space[current] = 0
+            if not cube and count == newborn:
+                new_space[current] = 1
+        space = {coord: cube for coord, cube in new_space.items() if cube}
+        yield space
+
+
 def solve(file_name: str):
     m = {'.': 0, '#': 1}
     initial_space = dict()
@@ -9,43 +33,12 @@ def solve(file_name: str):
         for x, char in enumerate(row.strip()):
             initial_space[x, y] = m[char]
 
-    space = {(x, y, 0): cube for (x, y), cube in initial_space.items()}
-    neighbors = list(product((-1, 0, 1), repeat=3))
-    for i in range(6):
-        new_space = space.copy()
-        coords_to_check = {
-            (nx+x, ny+y, nz+z)
-            for (x, y, z), cube in space.items() if cube
-            for nx, ny, nz in neighbors
-        }
-        for (x, y, z) in coords_to_check:
-            cube = space.get((x, y, z), 0)
-            count = sum(space.get((x+nx, y+ny, z+nz), 0) for nx, ny, nz in neighbors)
-            if cube and count not in (3, 4):  # 2, 3 but there is the active cube at 0,0,0
-                new_space[(x, y, z)] = 0
-            if not cube and count == 3:
-                new_space[(x, y, z)] = 1
-        space = {(x, y, z): cube for (x, y, z), cube in new_space.items() if cube}
-    print(sum(space.values()))
+    for d in (3, 4):
+        for i, space in enumerate(conway_n_d(initial_space, d)):
+            if i == 5:
+                break
+        print(f"{d}D: ", sum(space.values()))
 
-    space = {(x, y, 0, 0): cube for (x, y), cube in initial_space.items()}
-    neighbors = list(product((-1, 0, 1), repeat=4))
-    for i in range(6):
-        new_space = space.copy()
-        coords_to_check = {
-            (nx+x, ny+y, nz+z, nw+w)
-            for (x, y, z, w), cube in space.items() if cube
-            for nx, ny, nz, nw in neighbors
-        }
-        for (x, y, z, w) in coords_to_check:
-            cube = space.get((x, y, z, w), 0)
-            count = sum(space.get((x+nx, y+ny, z+nz, w+nw), 0) for nx, ny, nz, nw in neighbors)
-            if cube and count not in (3, 4):  # 2, 3 but there is the active cube at 0,0,0
-                new_space[(x, y, z, w)] = 0
-            if not cube and count == 3:
-                new_space[(x, y, z, w)] = 1
-        space = {(x, y, z, w): cube for (x, y, z, w), cube in new_space.items() if cube}
-    print(sum(space.values()))
 
 if __name__ == "__main__":
     solve(sys.argv[1] if len(sys.argv) > 1 else "input")
