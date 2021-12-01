041c5769c4a3a3280547d42adc8e80b9334b835c
Lorenzo Mele <lorenzo.mele@revolut.com>
2021-12-01 13:07:42 +0100

    2021 01 **

--- 2021/01/solve.py
+++ 2021/01/solve.py
@@ -1,10 +1,21 @@
 from sys import stdin
 from functools import reduce
 
+nums = list(map(int, stdin))
+
 prev = None
 increases = 0
-for num in map(int, stdin):
+for num in nums:
     if prev is not None:
         increases += num > prev
     prev = num
+print(increases)
+
+prev = None
+increases = 0
+for i in range(len(nums)-2):
+    s = sum(nums[i:i+3])
+    if prev is not None:
+        increases += s > prev
+    prev = s
 print(increases)
