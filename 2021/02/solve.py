d1cc780da53dc74d8cfd9f56a3d83ec82f659484
Lorenzo Mele <lorenzo.mele@revolut.com>
2021-12-02 10:08:11 +0100

    2021 02 ** more cleaning

--- 2021/02/solve.py
+++ 2021/02/solve.py
@@ -1,7 +1,4 @@
-from sys import stdin
-
 import sys
-print(sys.version)
 
 
 class Day02:
@@ -9,20 +6,20 @@ class Day02:
     def solve1(data: str) -> int:
         horizontal = depth = 0
         for line in data.splitlines():
-            # match line.split():
-            #     case ["forward", n]:
-            #         horizontal += int(n)
-            #     case ["down", n]:
-            #         depth += int(n)
-            #     case ["up", n]:
-            #         depth -= int(n)
-            direction, n = line.split()
-            if direction == "forward":
-                horizontal += int(n)
-            elif direction == "down":
-                depth += int(n)
-            elif direction == "up":
-                depth -= int(n)
+            match line.split():
+                case["forward", n]:
+                    horizontal += int(n)
+                case["down", n]:
+                    depth += int(n)
+                case["up", n]:
+                    depth -= int(n)
+            # direction, n = line.split()
+            # if direction == "forward":
+            #     horizontal += int(n)
+            # elif direction == "down":
+            #     depth += int(n)
+            # elif direction == "up":
+            #     depth -= int(n)
         return horizontal * depth
 
     @staticmethod
