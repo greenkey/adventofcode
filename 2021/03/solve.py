2be9e630a40b7ed3f0a24d972980a22290074c14
Lorenzo Mele <lorenzo.mele@revolut.com>
2021-12-03 20:39:35 +0100

    2021 03 **

--- 2021/03/solve.py
+++ 2021/03/solve.py
@@ -1,3 +1,4 @@
+from typing import Iterable
 from sys import stdin
 
 
@@ -20,7 +21,34 @@ class Day:
                 epsilon.append("1")
         return int(''.join(gamma), 2) * int(''.join(epsilon), 2)
 
+    @classmethod
+    def solve2(cls, data: str) -> int:
+        lines = data.splitlines()
+        oxygen = int(cls.oxygen(lines), 2)
+        co2 = int(cls.co2(lines), 2)
+        return oxygen * co2
+        
+    @classmethod
+    def oxygen(cls, lines: list[str]) -> str:
+        if len(lines) == 1:
+            return lines[0]
+        splitted = {"0": [], "1": []}
+        for line in lines:
+            splitted[line[0]].append(line[1:])
+        c = str(int(len(splitted["0"]) <= len(splitted["1"])))
+        return c + cls.oxygen(splitted[c])
+
+    @classmethod
+    def co2(cls, lines: list[str]) -> str:
+        if len(lines) == 1:
+            return lines[0]
+        splitted = {"0": [], "1": []}
+        for line in lines:
+            splitted[line[0]].append(line[1:])
+        c = str(int(len(splitted["0"]) > len(splitted["1"])))
+        return c + cls.co2(splitted[c])
 
 if __name__ == "__main__":
     data = stdin.read()
     print(Day.solve1(data))
+    print(Day.solve2(data))
--- 2021/03/test.py
+++ 2021/03/test.py
@@ -17,3 +17,13 @@ data = """00100
 
 def test_case_1():
     assert Day.solve1(data) == 198
+
+def test_oxygen():
+    assert Day.oxygen(data.splitlines()) == "10111"
+
+def test_co2():
+    assert Day.co2(data.splitlines()) == "01010"
+
+def test_case_2():
+    assert Day.solve2(data) == 230
+
