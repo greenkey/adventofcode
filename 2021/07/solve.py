f572c64d2a2e088ce5798c8331821db87c37bab4
Lorenzo Mele <lorenzo.mele@gmail.com>
2021-12-08 00:12:41 +0000

    2021 07 **

--- 2021/07/solve.py
+++ 2021/07/solve.py
@@ -1,6 +1,12 @@
 from collections import Counter, defaultdict
+from functools import lru_cache
 from sys import stdin
 from typing import Dict, List, Optional, Set, Tuple
+
+
+@lru_cache()
+def excl(n: int) -> int:
+    return sum(range(n + 1))
 
 
 class Day:
@@ -13,8 +19,15 @@ class Day:
             fuel = min(fuel, sum(abs(c - a) for c in self.crabs))
         return fuel
 
+    def solve2(self) -> int:
+        fuel = sum(excl(c) for c in self.crabs)
+        for a in range(1, max(self.crabs) + 1):
+            fuel = min(fuel, sum(excl(abs(c - a)) for c in self.crabs))
+        return fuel
+
 
 if __name__ == "__main__":
     data = stdin.read()
     d = Day(data)
     print(d.solve1())
+    print(d.solve2())
--- 2021/07/test.py
+++ 2021/07/test.py
@@ -5,3 +5,7 @@ data = """16,1,2,0,4,2,7,1,2,14"""
 
 def test_case_1():
     assert Day(data).solve1() == 37
+
+
+def test_case_2():
+    assert Day(data).solve2() == 168
