1c7f3f0fa8e935744742386d19fc306e352e1425
Lorenzo Mele <lorenzo.mele@octoenergy.com>
2022-12-02 09:01:32 +0100

    refactoring of day 1

--- 2022/solve_01.py
+++ 2022/solve_01.py
@@ -1,50 +1,29 @@
-from aocd.models import Puzzle
-
-puzzle = Puzzle(year=2022, day=1)
+import solve
 
 
-def get_elves(data):
-    elf = 0
+class Solution(solve.Solution):
+    answer_example_a = 24000
+    answer_example_b = 45000
 
-    for line in data.splitlines():
-        if line.strip() == "":
-            yield elf
-            elf = 0
-        else:
-            elf += int(line)
-    yield elf
+    def get_elves(self):
+        elf = 0
 
+        for line in self.data.splitlines():
+            if line.strip() == "":
+                yield elf
+                elf = 0
+            else:
+                elf += int(line)
+        yield elf
 
-def solve_a(data):
-    elves = list(get_elves(data))
-    return sorted(elves)[-1]
+    def solve_a(self):
+        elves = list(self.get_elves())
+        return sorted(elves)[-1]
 
-
-def solve_b(data):
-    elves = list(get_elves(data))
-    return sum(sorted(elves)[-3:])
-
-
-def test_a():
-    assert solve_a(puzzle.example_data) == 24000
+    def solve_b(self):
+        elves = list(self.get_elves())
+        return sum(sorted(elves)[-3:])
 
 
-def test_b():
-    assert solve_b(puzzle.example_data) == 45000
-
-
-def test_refactoring():
-    if puzzle.answered_a:
-        assert str(solve_a(puzzle.input_data)) == puzzle.answer_a
-    if puzzle.answered_b:
-        assert str(solve_b(puzzle.input_data)) == puzzle.answer_b
-
-
-if __name__ == "__main__":
-    if not puzzle.answered_a:
-        puzzle.answer_a = solve_a(puzzle.input_data)
-    print(f"A: {puzzle.answer_a}")
-
-    if not puzzle.answered_b:
-        puzzle.answer_b = solve_b(puzzle.input_data)
-    print(f"B: {puzzle.answer_b}")
+def test_ciao():
+    pass
