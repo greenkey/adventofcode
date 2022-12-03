a76487957ff5a9afb0297bac2372f57de4d7da06
Lorenzo Mele <lorenzo.mele@octoenergy.com>
2022-12-03 10:05:44 +0100

    2022 03 **

--- 2022/solve_03.py
+++ 2022/solve_03.py
@@ -3,7 +3,7 @@ from string import ascii_letters
 import solve
 
 answer_example_a = 157
-answer_example_b = None
+answer_example_b = 70
 
 
 def solve_a(data):
@@ -13,8 +13,14 @@ def solve_a(data):
         a, b = set(line[:size]), set(line[size:])
         dups.append((a & b).pop())
 
-    return sum([ascii_letters.find(c)+1 for c in dups])
+    return sum([ascii_letters.find(c) + 1 for c in dups])
 
 
 def solve_b(data):
-    return None
+    lines = [set(l) for l in data.splitlines()]
+    dups = []
+    for i in range(len(lines) // 3):
+        a, b, c = lines[i * 3 : i * 3 + 3]
+        dups.append((a & b & c).pop())
+
+    return sum([ascii_letters.find(c) + 1 for c in dups])
