15547bacc9b123e7b46e807821b3f87fbca72445
Lorenzo Mele <lorenzo.mele@octoenergy.com>
2022-12-04 09:27:21 +0100

    2022 04 *

---
+++ 2022/solve_04.py
@@ -0,0 +1,32 @@
+from string import ascii_letters
+
+import solve
+
+answer_example_a = 2
+answer_example_b = None
+
+
+def solve_a(data):
+    count = 0
+    for line in data.splitlines():
+        (a_start, a_end), (b_start, b_end) = [
+            [int(x) for x in range.split("-")] for range in line.split(",")
+        ]
+        if a_start <= b_start and a_end >= b_end:
+            count += 1
+        elif b_start <= a_start and b_end >= a_end:
+            count += 1
+    return count
+
+
+def solve_b(data):
+    return None
+
+
+def test_overlap():
+    data = "\n".join(["5-10,5-10"])
+    assert solve_a(data) == 1
+
+
+if __name__ == "__main__":
+    solve.main(2022, 4)
