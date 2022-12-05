0bce989360b14e1c758278eebf54b0029863290e
Lorenzo Mele <lorenzo.mele@octoenergy.com>
2022-12-05 08:29:00 +0100

    2022 05 **

--- 2022/solve_05.py
+++ 2022/solve_05.py
@@ -5,7 +5,7 @@ from string import ascii_letters
 import solve
 
 answer_example_a = "CMZ"
-answer_example_b = None
+answer_example_b = "MCD"
 
 
 cols_to_take = [1, 5, 9, 13, 17, 21, 25, 29, 33]
@@ -40,7 +40,30 @@ def solve_a(data):
 
 
 def solve_b(data):
-    return None
+    crates = defaultdict(list)
+    finished = False
+
+    first, second = data.split("\n\n")
+
+    for line in first.splitlines():
+        for crate, pos in enumerate(cols_to_take, start=1):
+            if pos > len(line):
+                break
+            item = line[pos].strip()
+            if item.isnumeric():
+                finished = True
+                break
+            if item:
+                crates[str(crate)].insert(0, item)
+        if finished:
+            break
+
+    for line in second.splitlines():
+        [(n, from_crate, to_crate)] = mask.findall(line)
+        crates[to_crate].extend(crates[from_crate][-int(n) :])
+        crates[from_crate] = crates[from_crate][: -int(n)]
+
+    return "".join([crates[str(i)].pop() for i in range(1, 10) if str(i) in crates])
 
 
 if __name__ == "__main__":
