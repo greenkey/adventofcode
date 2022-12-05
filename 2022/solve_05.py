e758bee29b240f7012c7d3d36023fc408e239eef
Lorenzo Mele <lorenzo.mele@octoenergy.com>
2022-12-05 08:17:29 +0100

    2022 05 *

---
+++ 2022/solve_05.py
@@ -0,0 +1,47 @@
+import re
+from collections import defaultdict
+from string import ascii_letters
+
+import solve
+
+answer_example_a = "CMZ"
+answer_example_b = None
+
+
+cols_to_take = [1, 5, 9, 13, 17, 21, 25, 29, 33]
+mask = re.compile(r"move ([0-9]+) from ([0-9]+) to ([0-9]+)")
+
+
+def solve_a(data):
+    crates = defaultdict(list)
+    finished = False
+
+    first, second = data.split("\n\n")
+
+    for line in first.splitlines():
+        for crate, pos in enumerate(cols_to_take, start=1):
+            if pos > len(line):
+                break
+            item = line[pos].strip()
+            if item.isnumeric():
+                finished = True
+                break
+            if item:
+                crates[str(crate)].insert(0, item)
+        if finished:
+            break
+
+    for line in second.splitlines():
+        [(n, from_crate, to_crate)] = mask.findall(line)
+        for _ in range(int(n)):
+            crates[to_crate].append(crates[from_crate].pop())
+
+    return "".join([crates[str(i)].pop() for i in range(1, 10) if str(i) in crates])
+
+
+def solve_b(data):
+    return None
+
+
+if __name__ == "__main__":
+    solve.main(2022, 5)
