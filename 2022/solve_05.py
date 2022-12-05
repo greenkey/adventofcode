0f048f3d0dd0e8c1cb2d0222c5edc5e515926632
Lorenzo Mele <lorenzo.mele@octoenergy.com>
2022-12-05 08:32:40 +0100

    2022 05 ** - refactoring

--- 2022/solve_05.py
+++ 2022/solve_05.py
@@ -12,58 +12,45 @@ cols_to_take = [1, 5, 9, 13, 17, 21, 25, 29, 33]
 mask = re.compile(r"move ([0-9]+) from ([0-9]+) to ([0-9]+)")
 
 
-def solve_a(data):
+def parse_crates(data) -> dict:
     crates = defaultdict(list)
-    finished = False
-
-    first, second = data.split("\n\n")
-
-    for line in first.splitlines():
+    for line in data.splitlines():
         for crate, pos in enumerate(cols_to_take, start=1):
             if pos > len(line):
                 break
             item = line[pos].strip()
             if item.isnumeric():
-                finished = True
-                break
+                return crates
             if item:
                 crates[str(crate)].insert(0, item)
-        if finished:
-            break
+
+
+def get_top_item(crates: dict) -> str:
+    return "".join([crates[str(i)].pop() for i in range(1, 10) if str(i) in crates])
+
+
+def solve_a(data):
+    first, second = data.split("\n\n")
+    crates = parse_crates(first)
 
     for line in second.splitlines():
         [(n, from_crate, to_crate)] = mask.findall(line)
         for _ in range(int(n)):
             crates[to_crate].append(crates[from_crate].pop())
 
-    return "".join([crates[str(i)].pop() for i in range(1, 10) if str(i) in crates])
+    return get_top_item(crates)
 
 
 def solve_b(data):
-    crates = defaultdict(list)
-    finished = False
-
     first, second = data.split("\n\n")
-
-    for line in first.splitlines():
-        for crate, pos in enumerate(cols_to_take, start=1):
-            if pos > len(line):
-                break
-            item = line[pos].strip()
-            if item.isnumeric():
-                finished = True
-                break
-            if item:
-                crates[str(crate)].insert(0, item)
-        if finished:
-            break
+    crates = parse_crates(first)
 
     for line in second.splitlines():
         [(n, from_crate, to_crate)] = mask.findall(line)
         crates[to_crate].extend(crates[from_crate][-int(n) :])
         crates[from_crate] = crates[from_crate][: -int(n)]
 
-    return "".join([crates[str(i)].pop() for i in range(1, 10) if str(i) in crates])
+    return get_top_item(crates)
 
 
 if __name__ == "__main__":
