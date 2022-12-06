a0dcfb7d36a0947038c31865fe2d140e0acf301f
Lorenzo Mele <lorenzo.mele@octoenergy.com>
2022-12-06 07:53:47 +0100

    2022 06 ** - refactoring

--- 2022/solve_06.py
+++ 2022/solve_06.py
@@ -1,49 +1,46 @@
 import pytest
+
 import solve
 
 answer_example_a = 7
 answer_example_b = 19
 
+examples = [
+    ("bvwbjplbgvbhsrlpgdmjqwftvncz", 5, 23),
+    ("nppdvjthqldpwncqszvftbrmjlhg", 6, 23),
+    ("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10, 29),
+    ("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11, 26),
+]
+
+
+def get_marker(sequence, length):
+    for i in range(len(sequence) - length):
+        if len(set(sequence[i : i + length])) == length:
+            break
+    return i + length
+
 
 def solve_a(sequence):
-    for i in range(len(sequence) - 4):
-        if len(set(sequence[i : i + 4])) == 4:
-            break
-    return i + 4
+    return get_marker(sequence, 4)
 
 
 def solve_b(sequence):
-    for i in range(len(sequence) - 14):
-        if len(set(sequence[i : i + 14])) == 14:
-            break
-    return i + 14
+    return get_marker(sequence, 14)
 
 
-# @pytest.mark.parametrize(
-#     "sequence, marker",
-#     [
-#         ("bvwbjplbgvbhsrlpgdmjqwftvncz", 5),
-#         ("nppdvjthqldpwncqszvftbrmjlhg", 6),
-#         ("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10),
-#         ("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11),
-#     ],
-# )
-# def test_find_marker(sequence, marker):
-#     assert solve_a(sequence) == marker
+def test_a():
+    for sequence, marker, _ in examples:
+        assert (
+            solve_a(sequence) == marker
+        ), f"{sequence=} expecting {marker=}, got {solve_a(sequence)}"
 
 
-# @pytest.mark.parametrize(
-#     "sequence, marker",
-#     [
-#         ("bvwbjplbgvbhsrlpgdmjqwftvncz", 23),
-#         ("nppdvjthqldpwncqszvftbrmjlhg", 23),
-#         ("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 29),
-#         ("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 26),
-#     ],
-# )
-# def test_find_marker_2(sequence, marker):
-#     assert solve_b(sequence) == marker
+def test_b():
+    for sequence, _, marker in examples:
+        assert (
+            solve_b(sequence) == marker
+        ), f"{sequence=} expecting {marker=}, got {solve_a(sequence)}"
 
 
 if __name__ == "__main__":
-    solve.main(2022, None)
+    solve.main(2022, 6)
