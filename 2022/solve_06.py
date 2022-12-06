d96192b01508dd5945dd0b925648bc38f65a3ffb
Lorenzo Mele <lorenzo.mele@octoenergy.com>
2022-12-06 07:48:54 +0100

    2022 06 **

--- 2022/solve_06.py
+++ 2022/solve_06.py
@@ -2,7 +2,7 @@ import pytest
 import solve
 
 answer_example_a = 7
-answer_example_b = None
+answer_example_b = 19
 
 
 def solve_a(sequence):
@@ -12,8 +12,11 @@ def solve_a(sequence):
     return i + 4
 
 
-def solve_b(data):
-    return None
+def solve_b(sequence):
+    for i in range(len(sequence) - 14):
+        if len(set(sequence[i : i + 14])) == 14:
+            break
+    return i + 14
 
 
 # @pytest.mark.parametrize(
@@ -27,6 +30,19 @@ def solve_b(data):
 # )
 # def test_find_marker(sequence, marker):
 #     assert solve_a(sequence) == marker
+
+
+# @pytest.mark.parametrize(
+#     "sequence, marker",
+#     [
+#         ("bvwbjplbgvbhsrlpgdmjqwftvncz", 23),
+#         ("nppdvjthqldpwncqszvftbrmjlhg", 23),
+#         ("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 29),
+#         ("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 26),
+#     ],
+# )
+# def test_find_marker_2(sequence, marker):
+#     assert solve_b(sequence) == marker
 
 
 if __name__ == "__main__":
