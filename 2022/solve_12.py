3381b2148aa8ebb29a484d9e746022f458065717
Lorenzo Mele <lorenzo.mele@octoenergy.com>
2022-12-12 08:40:25 +0100

    2022 12 **

--- 2022/solve_12.py
+++ 2022/solve_12.py
@@ -1,7 +1,7 @@
 import solve
 
 answer_example_a = 31
-answer_example_b = None
+answer_example_b = 29
 
 
 directions = [
@@ -28,37 +28,55 @@ class MyMap:
                 else:
                     self.mymap[x, y] = ord(c)
 
-    def next_step(self, from_x, from_y):
+    def next_step(self, from_x, from_y, reverse=False):
         cur = self.mymap[from_x, from_y]
 
         for dx, dy in directions:
             x = from_x + dx
             y = from_y + dy
-            if (next_p := self.mymap.get((x, y))) and next_p <= cur + 1:
-                yield x, y
+            if next_p := self.mymap.get((x, y)):
+                if reverse and next_p >= cur - 1:
+                    yield x, y
+                elif not reverse and next_p <= cur + 1:
+                    yield x, y
 
 
 def solve_a(data):
     mymap = MyMap(data)
     visited = set()
-    length = 0
     current_positions = set([mymap.origin])
 
-    i = 0
+    length = 0
     while mymap.goal not in current_positions:
         next_positions = set()
         for p in current_positions:
             next_positions.update(mymap.next_step(*p))
         current_positions = next_positions - visited
         visited.update(next_positions)
-        i += 1
+        length += 1
         print(f"{len(visited)=}", end="\r")
 
-    return i
+    return length
 
 
 def solve_b(data):
-    return None
+    mymap = MyMap(data)
+    visited = set()
+    current_positions = set([mymap.goal])
+    height = mymap.mymap[mymap.goal]
+
+    length = 0
+    while height > ord("a"):
+        next_positions = set()
+        for p in current_positions:
+            next_positions.update(mymap.next_step(*p, reverse=True))
+        current_positions = next_positions - visited
+        visited.update(next_positions)
+        length += 1
+        height = min(mymap.mymap[p] for p in next_positions)
+        print(f"{len(visited)=}", end="\r")
+
+    return length
 
 
 if __name__ == "__main__":
