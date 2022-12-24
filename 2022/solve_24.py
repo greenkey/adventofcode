04e7e4ae04604549192fc51f14652970c096a72b
Lorenzo Mele <lorenzo.mele@octoenergy.com>
2022-12-24 12:42:19 +0100

    2022 24 **

--- 2022/solve_24.py
+++ 2022/solve_24.py
@@ -4,7 +4,7 @@ import solve
 from utils import Point2D, print_map
 
 answer_example_a = 18
-answer_example_b = None
+answer_example_b = 54
 example_data = """#.######
 #>>.<^<#
 #.<..<<#
@@ -52,7 +52,7 @@ class BlizzardMap:
         self.y_size = y
 
     def get_points(self):
-        return {**self.walls, **{p: len(b) for p, b in self.blizzards.items()}}
+        return {**self.walls, **{p: str(len(b)) for p, b in self.blizzards.items()}}
 
     def print(self, then_wait=False, top_notes: str = "", additional_points=None):
         points = additional_points or {}
@@ -98,7 +98,7 @@ class BlizzardMap:
             return False
         if pos in self.walls:
             return False
-        if not 0 < pos.x <= self.x_size or not 0 < pos.y <= self.y_size:
+        if not 0 <= pos.x <= self.x_size or not 0 <= pos.y <= self.y_size:
             return False
         return True
 
@@ -106,18 +106,12 @@ class BlizzardMap:
 def solve_a(data):
     m = BlizzardMap()
     m.parse(data)
-    # m.print(then_wait=True)
     final_points = {m.start}
     minute = 0
     while True:
         m.move_blizzards()
         minute += 1
         final_points = {p for fp in final_points for p in m.possible_moves(fp)}
-        # m.print(
-        #     then_wait=True,
-        #     top_notes=f"{minute=}",
-        #     additional_points={p: "O" for p in final_points},
-        # )
         print(f"{minute=}, paths={len(final_points)}", end="\r")
         if m.end in final_points:
             break
@@ -125,7 +119,40 @@ def solve_a(data):
 
 
 def solve_b(data):
-    return None
+    m = BlizzardMap()
+    m.parse(data)
+
+    final_points = {m.start}
+    minute = 0
+    while True:
+        m.move_blizzards()
+        minute += 1
+        final_points = {p for fp in final_points for p in m.possible_moves(fp)}
+        print(f"{minute=}, paths={len(final_points)}     ", end="\r")
+        if m.end in final_points:
+            break
+
+    # back
+    final_points = {m.end}
+    while True:
+        m.move_blizzards()
+        minute += 1
+        final_points = {p for fp in final_points for p in m.possible_moves(fp)}
+        print(f"{minute=}, paths={len(final_points)}     ", end="\r")
+        if m.start in final_points:
+            break
+
+    # forth again
+    final_points = {m.start}
+    while True:
+        m.move_blizzards()
+        minute += 1
+        final_points = {p for fp in final_points for p in m.possible_moves(fp)}
+        print(f"{minute=}, paths={len(final_points)}     ", end="\r")
+        if m.end in final_points:
+            break
+
+    return minute
 
 
 if __name__ == "__main__":
